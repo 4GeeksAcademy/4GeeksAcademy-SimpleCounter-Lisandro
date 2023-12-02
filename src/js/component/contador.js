ffef7a326aa3f582427f9d958ecc9f5cd61eee46
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2023-12-02 11:01:10 +0000

    Contador

--- src/js/component/contador.js
+++ src/js/component/contador.js
@@ -2,56 +2,61 @@ import React from "react";
 
 function Contador ({digito1, digito2, digito3, digito4, digito5, digito6,}){
     return(
-<div class="row bg bg-black p-4">
+<div class="row bg bg-black p-4 justify-content-center">
 
-<div class="col-sm-1 mb-3 mb-sm-0 bg-info">
-<i class="fa fa-clock"></i>
-</div>
+<div class="col-sm-1 mb-3 mb-sm-0">
+    <div class="card">
+      <div class="card-body bg-info border rounded">
+        <p class="card-text text-dark display-2"><i class="fa fa-clock"></i></p>
+        </div>
+        </div>
+        </div>
+
 
   <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito6}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white justify-conten display-2"><strong>{digito6}</strong></p>
         </div>
         </div>
         </div>
 
         <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito5}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white display-2"><strong>{digito5}</strong></p>
         </div>
         </div>
         </div>
 
         <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito4}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white display-2"><strong>{digito4}</strong></p>
         </div>
         </div>
         </div>
 
         <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito3}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white display-2"><strong>{digito3}</strong></p>
         </div>
         </div>
         </div>
 
         <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito2}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white display-2"><strong>{digito2}</strong></p>
         </div>
         </div>
         </div>
 
         <div class="col-sm-1 mb-3 mb-sm-0">
     <div class="card">
-      <div class="card-body bg-info">
-        <p class="card-text text-white"><strong>{digito1}</strong></p>
+      <div class="card-body bg-info object-fit-fill border rounded">
+        <p class="card-text text-white display-2"><strong>{digito1}</strong></p>
         </div>
         </div>
         </div>
