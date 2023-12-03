3b593f73c25fd194142e0bd9ec08ca7353a61914
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2023-12-03 14:29:59 +0000

    Contador terminado

--- src/js/component/contador.js
+++ src/js/component/contador.js
@@ -1,65 +1,18 @@
 import React from "react";
 
-function Contador ({digito1, digito2, digito3, digito4, digito5, digito6,}){
-    return(
-<div class="row bg bg-black p-4 justify-content-center">
+function Contador ({digito1, digito2, digito3, digito4, digito5, digito6}){
 
-<div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info border rounded">
+
+  const digitos=[digito1, digito2, digito3, digito4, digito5, digito6]
+    return(
+<div class="row bg bg-black p-4" style={{justifyContent:"space-between"}}>
+
+        <div class="col-sm-1 mb-3 mb-sm-0 card card-body bg-info border rounded" style={{marginRight:50, marginLeft:50}}>
         <p class="card-text text-dark display-2"><i class="fa fa-clock"></i></p>
         </div>
-        </div>
-        </div>
-
-
-  <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white justify-conten display-2"><strong>{digito6}</strong></p>
-        </div>
-        </div>
-        </div>
-
-        <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white display-2"><strong>{digito5}</strong></p>
-        </div>
-        </div>
-        </div>
-
-        <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white display-2"><strong>{digito4}</strong></p>
-        </div>
-        </div>
-        </div>
-
-        <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white display-2"><strong>{digito3}</strong></p>
-        </div>
-        </div>
-        </div>
-
-        <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white display-2"><strong>{digito2}</strong></p>
-        </div>
-        </div>
-        </div>
-
-        <div class="col-sm-1 mb-3 mb-sm-0">
-    <div class="card">
-      <div class="card-body bg-info object-fit-fill border rounded">
-        <p class="card-text text-white display-2"><strong>{digito1}</strong></p>
-        </div>
-        </div>
-        </div>
+{digitos.reverse().map((digito)=> (<div class="col-sm-1 mb-3 mb-sm-0 card card-body bg-info border rounded" style={{marginRight:50}}>
+        <p class="card-text text-white display-2"><strong>{digito}</strong></p>
+        </div>))}
  </div>
  );
 }
